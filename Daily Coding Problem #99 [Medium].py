006cb061c378693bd2ba1a7dccd6b056b43254e2
hello-klol <hello@klol.tech>
2020-11-24 17:00:26 +0000

    Daily Coding #99 minor changes to handling collections
    Signed-off-by: K-lol <k.a.clark87@gmail.com>

--- Daily Coding Problem #99 [Medium].py
+++ Daily Coding Problem #99 [Medium].py
@@ -3,21 +3,21 @@
 # For example, given [100, 4, 200, 1, 3, 2], the longest consecutive element sequence is [1, 2, 3, 4]. Return its length: 4.
 #
 # Your algorithm should run in O(n) complexity.
+import unittest
+
 
 def get_longest_consec(my_arr):
     consecutive = []
     my_arr.sort()
     length_count = 1
-    for i, e in enumerate(my_arr[:-1]):
-        if my_arr[i + 1] - e == 1:
+    pair_up = zip(my_arr[:-1], my_arr[1:])
+    for i, j in pair_up:
+        if j - i == 1:
             length_count += 1
         else:
             consecutive.append(length_count)
             length_count = 1
     return max(consecutive)
-
-
-import unittest
 
 
 class TestConsec(unittest.TestCase):
